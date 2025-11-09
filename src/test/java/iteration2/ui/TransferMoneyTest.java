025580ba0dff4a0100d08c55adf7d49b3bd40087
RustamMust <rustammustavaev@gmail.com>
2025-11-09 12:12:29 +0500

    homework: add TransferMoneyTest

---
+++ src/test/java/iteration2/ui/TransferMoneyTest.java
@@ -0,0 +1,271 @@
+package iteration2.ui;
+
+import assertions.BalanceAssertions;
+import com.codeborne.selenide.*;
+import generators.RandomData;
+import helpers.AccountStepsHelper;
+import models.CreateUserRequest;
+import models.LoginUserRequest;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import requests.skeleton.Endpoint;
+import requests.skeleton.requesters.CrudRequester;
+import requests.steps.AccountsSteps;
+import requests.steps.AdminSteps;
+import specs.RequestSpecs;
+import specs.ResponseSpecs;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TransferMoneyTest {
+    protected SoftAssertions softly;
+
+    @BeforeAll
+    public static void setupSelenoid() {
+        //Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.baseUrl = "http://localhost:3000";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+
+        Configuration.browserCapabilities.setCapability("selenoid:options",
+                Map.of("enableVNC", true, "enableLog", true)
+        );
+    }
+
+    @BeforeEach
+    public void setupSoftAssertions() {
+        softly = new SoftAssertions();
+    }
+
+    @Test
+    public void userCanTransferMoneyTest() {
+        // ШАГИ ПО НАСТРОЙКЕ ОКРУЖЕНИЯ
+        // 1 - Create sender user
+        CreateUserRequest senderUser = AdminSteps.createUser();
+
+        // 2 - Create sender account
+        var senderSpec = RequestSpecs.authAsUser(senderUser.getUsername(), senderUser.getPassword());
+        AccountsSteps.createAccount(senderSpec);
+
+        // 3 - Get sender account id from profile
+        int senderAccountId = AccountStepsHelper.getAccountId(senderSpec);
+
+        // 4 - Deposit to sender account
+        int randomBalance = RandomData.getRandomBalance();
+        AccountsSteps.depositMoney(senderSpec, senderAccountId, randomBalance);
+
+        // 5 - Create receiver user
+        CreateUserRequest receiverUser = AdminSteps.createUser();
+
+        // 6 - Create receiver account
+        var receiverSpec = RequestSpecs.authAsUser(receiverUser.getUsername(), receiverUser.getPassword());
+        AccountsSteps.createAccount(receiverSpec);
+
+        // 7 - Get receiver account name
+        String receiverAccountName = AccountStepsHelper.getAccountName(receiverSpec);
+
+        // 8 - Get sender and receiver balances
+        double senderBalanceBefore = AccountStepsHelper.getBalance(senderSpec);
+        double receiverBalanceBefore = AccountStepsHelper.getBalance(receiverSpec);
+
+        // 9 - Get user auth header
+        String userAuthHeader = new CrudRequester(
+                RequestSpecs.unauthSpec(),
+                Endpoint.LOGIN,
+                ResponseSpecs.requestReturnsOK())
+                .post(LoginUserRequest.builder().username(senderUser.getUsername()).password(senderUser.getPassword()).build())
+                .extract()
+                .header("Authorization");
+
+        // 10 - Open page
+        Selenide.open("/");
+
+        // 11 - Set authToken to localStorage
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", userAuthHeader);
+
+        // 12 - Open dashboard page
+        Selenide.open("/dashboard");
+
+        // ШАГИ ТЕСТА
+        // 13 - Click on "Make a Transfer" button
+        $(byText("\uD83D\uDD04 Make a Transfer")).click();
+
+        // 14 - Click on "Choose an account" dropdown
+        SelenideElement accountSelect = $x("//option[normalize-space(text())='-- Choose an account --']/ancestor::select");
+
+        // 15 - Select the first account
+        accountSelect.selectOption(1);
+
+        // 16 - Find "Enter recipient name" input
+        SelenideElement enterRecipientInput = $("[placeholder='Enter recipient name']");
+
+        // 17 - Clear "Enter recipient name" input
+        enterRecipientInput.shouldBe(visible).clear();
+
+        // 18 - Set value to "Enter recipient name" input
+        enterRecipientInput.setValue(receiverUser.getUsername());
+
+        // 19 - Find "Enter recipient account number" input
+        SelenideElement enterRecipientAccountNumberInput = $("[placeholder='Enter recipient account number']");
+
+        // 20 - Clear "Enter recipient account number" input
+        enterRecipientAccountNumberInput.shouldBe(visible).clear();
+
+        // 21 - Set value to "Enter recipient account number" input
+        enterRecipientAccountNumberInput.setValue(receiverAccountName);
+
+        // 22 - Find "Enter amount" input
+        SelenideElement amountInput = $("[placeholder='Enter amount']");
+
+        // 23 - Clear "Enter amount" input
+        amountInput.shouldBe(visible).clear();
+
+        // 24 - Set value to "Enter amount" input
+        int transferAmount = 1;
+        amountInput.setValue(String.valueOf(transferAmount));
+
+        // 25 - Click on "Confirm details are correct" checkbox
+        SelenideElement confirmCheckbox = $("#confirmCheck");
+        confirmCheckbox.shouldBe(visible).click();
+
+        // 26 - Click on "Send Transfer" button
+        $(byText("\uD83D\uDE80 Send Transfer")).click();
+
+        // 27 - Assert that Successfully transferred from UI
+        Alert alert = switchTo().alert();
+        String alertText = alert.getText();
+        assertThat(alertText).contains("✅ Successfully transferred");
+        alert.accept();
+
+        // 28 - Assert that after clicking OK, the user still on "Make a Transfer" page
+        $(Selectors.byText("\uD83D\uDD04 Make a Transfer")).shouldBe(Condition.visible);
+
+        // 29 - Get sender and receiver balances
+        double senderBalanceAfter = AccountStepsHelper.getBalance(senderSpec);
+        double receiverBalanceAfter = AccountStepsHelper.getBalance(receiverSpec);
+
+        // 30 - Assert balances via API
+        BalanceAssertions.assertBalanceDecreasedBy(softly, senderBalanceBefore, senderBalanceAfter, transferAmount);
+        BalanceAssertions.assertBalanceIncreasedBy(softly, receiverBalanceBefore, receiverBalanceAfter, transferAmount);
+    }
+
+    @Test
+    public void userCannotTransferMoneyTest() {
+        // ШАГИ ПО НАСТРОЙКЕ ОКРУЖЕНИЯ
+        // 1 - Create sender user
+        CreateUserRequest senderUser = AdminSteps.createUser();
+
+        // 2 - Create sender account
+        var senderSpec = RequestSpecs.authAsUser(senderUser.getUsername(), senderUser.getPassword());
+        AccountsSteps.createAccount(senderSpec);
+
+        // 3 - Get sender account id from profile
+        int senderAccountId = AccountStepsHelper.getAccountId(senderSpec);
+
+        // 4 - Deposit to sender account
+        int randomBalance = RandomData.getRandomBalance();
+        AccountsSteps.depositMoney(senderSpec, senderAccountId, randomBalance);
+
+        // 5 - Create receiver user
+        CreateUserRequest receiverUser = AdminSteps.createUser();
+
+        // 6 - Create receiver account
+        var receiverSpec = RequestSpecs.authAsUser(receiverUser.getUsername(), receiverUser.getPassword());
+        AccountsSteps.createAccount(receiverSpec);
+
+        // 7 - Get receiver account name
+        String receiverAccountName = AccountStepsHelper.getAccountName(receiverSpec);
+
+        // 8 - Get sender and receiver balances
+        double senderBalanceBefore = AccountStepsHelper.getBalance(senderSpec);
+        double receiverBalanceBefore = AccountStepsHelper.getBalance(receiverSpec);
+
+        // 9 - Get user auth header
+        String userAuthHeader = new CrudRequester(
+                RequestSpecs.unauthSpec(),
+                Endpoint.LOGIN,
+                ResponseSpecs.requestReturnsOK())
+                .post(LoginUserRequest.builder().username(senderUser.getUsername()).password(senderUser.getPassword()).build())
+                .extract()
+                .header("Authorization");
+
+        // 10 - Open page
+        Selenide.open("/");
+
+        // 11 - Set authToken to localStorage
+        executeJavaScript("localStorage.setItem('authToken', arguments[0]);", userAuthHeader);
+
+        // 12 - Open dashboard page
+        Selenide.open("/dashboard");
+
+        // ШАГИ ТЕСТА
+        // 13 - Click on "Make a Transfer" button
+        $(byText("\uD83D\uDD04 Make a Transfer")).click();
+
+        // 14 - Click on "Choose an account" dropdown
+        SelenideElement accountSelect = $x("//option[normalize-space(text())='-- Choose an account --']/ancestor::select");
+
+        // 15 - Select the first account
+        accountSelect.selectOption(1);
+
+        // 16 - Find "Enter recipient name" input
+        SelenideElement enterRecipientInput = $("[placeholder='Enter recipient name']");
+
+        // 17 - Clear "Enter recipient name" input
+        enterRecipientInput.shouldBe(visible).clear();
+
+        // 18 - Set value to "Enter recipient name" input
+        enterRecipientInput.setValue(receiverUser.getUsername());
+
+        // 19 - Find "Enter recipient account number" input
+        SelenideElement enterRecipientAccountNumberInput = $("[placeholder='Enter recipient account number']");
+
+        // 20 - Clear "Enter recipient account number" input
+        enterRecipientAccountNumberInput.shouldBe(visible).clear();
+
+        // 21 - Set value to "Enter recipient account number" input
+        enterRecipientAccountNumberInput.setValue(receiverAccountName);
+
+        // 22 - Find "Enter amount" input
+        SelenideElement amountInput = $("[placeholder='Enter amount']");
+
+        // 23 - Clear "Enter amount" input
+        amountInput.shouldBe(visible).clear();
+
+        // 24 - Set value to "Enter amount" input
+        int transferAmount = 20000;
+        amountInput.setValue(String.valueOf(transferAmount));
+
+        // 25 - Click on "Confirm details are correct" checkbox
+        SelenideElement confirmCheckbox = $("#confirmCheck");
+        confirmCheckbox.shouldBe(visible).click();
+
+        // 26 - Click on "Send Transfer" button
+        $(byText("\uD83D\uDE80 Send Transfer")).click();
+
+        // 27 - Assert validation message from UI
+        Alert alert = switchTo().alert();
+        String alertText = alert.getText();
+        assertThat(alertText).contains("❌ Error: Transfer amount cannot exceed 10000");
+        alert.accept();
+
+        // 28 - Assert that after clicking OK, the user still on "Make a Transfer" page
+        $(Selectors.byText("\uD83D\uDD04 Make a Transfer")).shouldBe(Condition.visible);
+
+        // 29 - Get sender and receiver balances
+        double senderBalanceAfter = AccountStepsHelper.getBalance(senderSpec);
+        double receiverBalanceAfter = AccountStepsHelper.getBalance(receiverSpec);
+
+        // 30 - Assert balances did not change via API
+        BalanceAssertions.assertBalanceUnchanged(softly, senderBalanceBefore, senderBalanceAfter);
+        BalanceAssertions.assertBalanceUnchanged(softly, receiverBalanceBefore, receiverBalanceAfter);
+    }
+}
